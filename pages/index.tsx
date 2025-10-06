7f614647e2bbb306175c11f2fd9d055f693e7906
citymedialab <g.c.christopoulos@gmail.com>
2025-10-06 18:54:23 +0300

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{minHeight:'100vh',display:'grid',placeItems:'center',fontSize:24}}>
+      <div>✅ citymedialab — pages/index.tsx</div>
+    </div>
+  )
+}
