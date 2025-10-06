9f577a8efdb8fc2a4ad1ac41eb79bf1aa926a28f
citymedialab <g.c.christopoulos@gmail.com>
2025-10-06 19:46:25 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{minHeight:'100vh',display:'grid',placeItems:'center',fontSize:24}}>
-      <div>✅ citymedialab — pages/index.tsx</div>
-    </div>
-  )
-}
+
