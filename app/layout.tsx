b01388ddab3709f266403c563bd0f88f02561b6b
citymedialab <g.c.christopoulos@gmail.com>
2025-10-09 12:43:07 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Header from '@/components/Header'
+import Splash from '@/components/Splash'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'City Media Lab',
-  description: 'City Media Lab',
+  title: 'QR City',
+  description: 'QR-powered city experiences',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="el">
       <body className={inter.className}>
+        <Splash />
+        <Header />
         {children}
       </body>
     </html>
